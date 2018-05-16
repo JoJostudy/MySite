cc654bd08e3290c8d7dd96d78a2d2e29082f5494
Rivarrl <1049793871@qq.com>
2018-05-16 23:11:19 +0800

    no message

--- MySite/settings.py
+++ MySite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Rivarrl.apps.RivarrlConfig',
-    'Rivarrl'
+    'Rivarrl.apps.RivarrlConfig'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
